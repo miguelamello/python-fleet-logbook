c41ca02da20890b6e058f74220af85224bf2981f
Miguel Angelo Mello] <miguelangelomello@gmail.com>
2023-06-07 16:21:27 -0300

    2023-06-07 16:21:27 by Miguel

--- ingestor/ingestor.py
+++ ingestor/ingestor.py
@@ -6,45 +6,52 @@ import time
 from datetime import datetime, timezone
 import csv
 import random
+import json
+from logger import Logger
+from mongo import Mongo
 
 class Ingestor:
   def __init__(self):
+    self.logger = Logger()
+    self.mongo = Mongo()
     self.vdr_data = []
     self.poll_vdr_data()
-
-  # Handles errors output
-  def error_handler(err):
-    # Print the error message to the console 
-    # but in production you would probably 
-    # want to log to a database instead
-    print(err)
 
   # Read nmea data from file and store in a list
   # to simulate data from the VDR
   def poll_vdr_data(self):
-    filename = "nmeadata.log"
-    with open(filename, "r") as file:
-      reader = csv.reader(file)
-      self.vdr_data = list(reader)
+    try:
+      filename = "nmeadata.log"
+      with open(filename, "r") as file:
+        reader = csv.reader(file)
+        self.vdr_data = list(reader)
+    except Exception:
+      self.logger.store()
 
   # Get a random line from the list of VDR data
-  def get_vdr_sentence(self):
-    index = random.randint(0, len(self.vdr_data) - 1)
-    data = {
-      "source": self.vdr_data[index][0],
-      "latitude": self.vdr_data[index][1] + self.vdr_data[index][2],
-      "longitude": self.vdr_data[index][3] + self.vdr_data[index][4],
-      "utctime": self.vdr_data[index][5]
-    }
-    return data
+  def get_vdr_sentence(self): 
+    if len(self.vdr_data) == 0:
+      return None
+    else:
+      index = random.randint(0, len(self.vdr_data) - 1)
+      data = {
+        "source": self.vdr_data[index][0],
+        "latitude": self.vdr_data[index][1] + self.vdr_data[index][2],
+        "longitude": self.vdr_data[index][3] + self.vdr_data[index][4],
+        "utctime": self.vdr_data[index][5]
+      }
+      return data
 
   def format_sentence(self, sentence):
-    timestamp = datetime.now().isoformat()
-    data = {
-      'timestamp': timestamp,
-      'metadata': sentence
-    }
-    return data
+    if len(sentence) == 0:
+      return None
+    else:
+      timestamp = datetime.utcnow()
+      data = {
+        'timestamp': timestamp,
+        'metadata': sentence
+      }
+      return data
 
   def start(self):
     while True:
@@ -52,12 +59,12 @@ class Ingestor:
       if sentence: 
         timeserie = self.format_sentence(sentence)
         if timeserie:
-          print(timeserie)
           # Insert the timeseries into the database
-          #self.mongo.insert_one(timeserie)
+          self.mongo.store_one(timeserie)
       time.sleep(10)  # Sleep for 10 second before collecting the next reading
 
   def stop(self):
-    pass
     # Close the connection to the MongoDB server
-    #self.mongo.close()
+    self.mongo.close()
+
+    
---
+++ ingestor/logger.py
@@ -0,0 +1,17 @@
+import logging
+import traceback
+
+class Logger:
+  def __init__(self):
+    self.logger = logging.getLogger(__name__)
+    self.logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('ingestor.log')
+    self.logger.addHandler(file_handler)
+
+  # Handles errors output
+  # Print the error message to a logfile  
+  # but in production you should probably 
+  # want to log to a database instead
+  def store(self):
+    self.logger.warning(traceback.format_exc())
+
